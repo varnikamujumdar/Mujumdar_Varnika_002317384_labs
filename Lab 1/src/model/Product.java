dad2b8c0ea5eb29b0b4c4b2babcfa0074e5c47da
VarnikaI562978 <mujumdar.v@northeastern.edu>
2024-09-15 07:31:24 +0530

    added Lab 1 folder

---
+++ Lab 1/src/model/Product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Varnika Mujumdar
+ */
+public class Product {
+    
+    String name;
+    String description;
+    String availaibility;
+    String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailaibility() {
+        return availaibility;
+    }
+
+    public void setAvailaibility(String availaibility) {
+        this.availaibility = availaibility;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+}
+
